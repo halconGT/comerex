3dc9d73ed25bdbf2137472e362a0317b91acb0ae
halconGT <61984892+halconGT@users.noreply.github.com>
2020-03-09 16:19:29 -0600

    primer commit
    primera página y un css con elemento tabla @halcongt

---
+++ proveedores-ext1.php
@@ -0,0 +1,31 @@
+<?php 
+echo 'Hello World!';
+
+?>
+
+BANGUAT 
+  <br>
+<table >
+  <tr>
+   <td>
+    Dólar estadounidense
+   </td>
+   <td>
+    Quetzales
+   </td>
+  </tr>
+
+  <tr>
+   <td>
+    1.00
+   </td>
+   <td>
+    7.5845
+   </td>
+  </tr>
+  </table>
+/*<input type="button" value="Alibaba">*/
+<a href="alibaba.com">Alibaba</a>
+  
+¿cómo comprar en el extranjero?
+  ¿cómo comprar en el extranjero?
